2960c8c8b286fa8bbb4f93cd205c7dd5f061bf48
Maria Rjabtseva <nkdsnake@tlu.ee>
2025-04-02 12:51:52 +0000

    added comparing method, but test is failing bruh

--- dz/__tests__/workout_test.ts
+++ dz/__tests__/workout_test.ts
@@ -1,6 +1,6 @@
 import { Exercise, WorkoutTracker} from "../workout";
  /*name: string, musclegroup: string, weight: number, sets:number, reps: number*/
-let emptyTtracker = new WorkoutTracker('a', '01-01-2025');
+ let emptyTtracker = new WorkoutTracker('a', '01-01-2025');
 
 let chest1 = new Exercise('incline bench press', 'chest', 20, 3, 8);
 let chest2 = new Exercise('machine chest fly', 'chest', 20, 3, 8);
@@ -8,9 +8,10 @@ let biceps = new Exercise('cable hammer curl', 'biceps', 30, 2, 12);
 let forearms = new Exercise('forearms press machine', 'forearms', 27.5, 2, 20);
 
 let upperbodyDay = new WorkoutTracker('upperbody', '01-04-2025');
-upperbodyDay.addExercise(chest2);
+
 upperbodyDay.addExercise(biceps);
 upperbodyDay.addExercise(forearms);
+//beforeEach()
 
  test("should initialize with an empty exercise list", () => {
     expect(emptyTtracker.exerciseList).toEqual([]);
@@ -29,4 +30,16 @@ test("should remove an exercise", () => {
 
 test("should count total reps", () => {
     expect(upperbodyDay.getTotalReps()).toBe(88);
+});
+
+test("should not compare, beacuse previous exercise does not exist", () => {
+    expect(emptyTtracker.compareResult(upperbodyDay.exerciseList[0])).toBe("does not have exercise in previous workout");
+});
+
+test("workload should be increased", () => {
+    let chest3 = new Exercise('machine chest fly', 'chest', 30, 3, 7); // Новое упражнение с измененным объемом
+
+    let result = upperbodyDay.compareResult(chest3);
+    // Проверяем, что сообщение содержит "workload has increased"
+    expect(result).toContain("workload has increased");
 });
